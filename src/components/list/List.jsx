72f5f83cf9c37f0ede9b2d870998b1b101dddef1
marifrahmawan <m1600018181@webmail.uad.ac.id>
2021-08-29 17:46:38 +0800

    new scrollbar for movie row

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,60 +1,16 @@
-import React, { useRef, useState, useEffect } from 'react';
-
-import {
-  ArrowBackIosOutlined,
-  ArrowForwardIosOutlined,
-} from '@material-ui/icons';
+import React from 'react';
 
 import ListItem from '../listItem/ListItem';
-import useWindowDimensions from './useWindowDimension';
 
 import './list.scss';
 
 //* List Component
 const List = () => {
-  const [slideNumber, setSlideNumber] = useState(1);
-  const [showSlider, setShowSlider] = useState(true);
-  const { width } = useWindowDimensions();
-  const listRef = useRef();
-
-  useEffect(() => {
-    const sliderBtnShowing = setTimeout(() => {
-      setShowSlider(false);
-    }, 250);
-
-    return () => {
-      setShowSlider(true);
-      clearTimeout(sliderBtnShowing);
-    };
-  }, [slideNumber]);
-
-  const handleClick = (direction) => {
-    const howManyBox = width / 230;
-    let distance = listRef.current.getBoundingClientRect().x - 50;
-
-    if (direction === 'right' && slideNumber <= 15 - howManyBox) {
-      setSlideNumber(slideNumber + 1);
-      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
-    }
-
-    if (direction === 'left' && slideNumber > 1) {
-      setSlideNumber(slideNumber - 1);
-      listRef.current.style.transform = `translateX(${230 + distance}px)`;
-    }
-  };
-
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined
-          className="sliderArrow left"
-          onClick={handleClick.bind(null, 'left')}
-          style={{
-            display: slideNumber === 1 || showSlider === true ? 'none' : '',
-          }}
-        />
-        <div className="container" ref={listRef}>
+        <div className="container">
           <ListItem />
           <ListItem />
           <ListItem />
@@ -70,13 +26,6 @@ const List = () => {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlined
-          className="sliderArrow right"
-          onClick={handleClick.bind(null, 'right')}
-          style={{
-            display: showSlider === true ? 'none' : '',
-          }}
-        />
       </div>
     </div>
   );
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import {
+  Add,
+  PlayArrow,
+  ThumbDownAltOutlined,
+  ThumbUpAltOutlined,
+} from '@material-ui/icons';
+
 import './list-item.scss';
 
 const ListItem = () => {
@@ -9,6 +16,20 @@ const ListItem = () => {
         src="https://fs.genpi.co/uploads/data/images/doctorstrange.jpg"
         alt="Movie"
       />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow />
+          <Add />
+          <ThumbUpAltOutlined />
+          <ThumbDownAltOutlined />
+        </div>
+        <div className="itemInfoTop">
+          <span>1 hour 40 mins</span>
+          <span>+16</span>
+          <span>2019</span>
+        </div>
+        <div className="genre">Action</div>
+      </div>
     </div>
   );
 };
